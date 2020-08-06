a7d07ed3381a4d7ca9399fe705529d33914fb771
ukrgerri4 <ukrgerri4@gmail.com>
2020-08-06 23:32:49 +0300

    f com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+import Peer from 'peerjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TestWebRTC';
+
+  ngOnInit(): void {
+    const guid = uuidv4();
+    const per = new Peer(guid);
+  }
+
 }
